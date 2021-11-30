eecfe09808042e108baaec120e2d709b9a322aa9
Gabriel Valverde <gabriel_gom@hotmail.com>
2021-11-30 00:07:46 -0300

    Change: Using enzyme on App.test.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { shallow } from 'enzyme';
+
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('should render without any error', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.isEmptyRender()).toBe(false);
 });
