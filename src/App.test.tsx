04604c857a35b2f9d184224c7de72905e233bca5
Gabriel Valverde <gabriel_gom@hotmail.com>
2021-12-02 23:27:42 -0300

    Add: App loading, error and success logic

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,14 @@
 import { shallow } from 'enzyme';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 import App from './App';
 
 test('should render without any error', () => {
-  const wrapper = shallow(<App />);
+  const wrapper = shallow(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+  );
   expect(wrapper.isEmptyRender()).toBe(false);
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,42 @@
 import './App.css';
+import { useEffect } from 'react';
+import { RootState } from './redux/store';
+import { useDispatch, useSelector } from 'react-redux';
+import FadeIn from 'react-fade-in';
+import { fetchLaunches } from './redux/modules/launches';
+
+import MainContainer from './components/MainContainer/MainContainer';
+import FetchError from './components/FetchError/FetchError';
+import FetchLoading from './components/FetchLoading/FetchLoading';
 
 const App: React.FC = () => {
-  return <div>Hello World</div>;
+  const dispatch = useDispatch();
+  const launchesState = useSelector((state: RootState) => state.launches);
+  const { fetchStatus } = launchesState;
+
+  useEffect(() => {
+    dispatch(fetchLaunches());
+  }, []);
+
+  return (
+    <MainContainer>
+      {fetchStatus.hasError && (
+        <FadeIn>
+          <FetchError />
+        </FadeIn>
+      )}
+
+      {fetchStatus.isLoading && !fetchStatus.hasError && (
+        <FadeIn>
+          <FetchLoading />
+        </FadeIn>
+      )}
+
+      {!fetchStatus.isLoading && !fetchStatus.hasError && (
+        <FadeIn>Hello World!</FadeIn>
+      )}
+    </MainContainer>
+  );
 };
 
 export default App;
