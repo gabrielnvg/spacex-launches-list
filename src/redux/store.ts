7fd8a397bd8f85241bfd69b19a8efe98bac4c464
Gabriel Valverde <gabriel_gom@hotmail.com>
2021-12-02 22:45:15 -0300

    Fix: Redux store composeEnhancers

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,14 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import launches from './modules/launches';
+import filtersDrawer from './modules/filtersDrawer';
+
+// @ts-ignore
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
-  combineReducers({ launches }),
-  applyMiddleware(thunk),
+  combineReducers({ launches, filtersDrawer }),
+  composeEnhancers(applyMiddleware(thunk)),
 );
 
 export default store;
