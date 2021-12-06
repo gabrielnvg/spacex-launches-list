f3ad4da02670e245a544c05f047d9c4116dd8413
Gabriel Gomes <gabriel_gom@hotmail.com>
2021-12-06 17:43:58 -0200

    Add: Launch mock

---
+++ __mocks__/launch.ts
@@ -0,0 +1,13 @@
+const launch = {
+  flightNumber: 1,
+  missionName: '',
+  launchYear: 1,
+  launchDateUtc: '',
+  launchSuccess: true,
+  upcoming: false,
+  rocketName: '',
+  imageUrl: '',
+  isFavourite: false,
+};
+
+export default launch;
