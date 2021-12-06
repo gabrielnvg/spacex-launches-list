d1f6fb2cb127aebe97b75ad11227878fba63b5de
Gabriel Gomes <gabriel_gom@hotmail.com>
2021-12-06 17:19:45 -0200

    Add: Filters component test

--- src/components/MainContainer/FiltersDrawer/Filters/Filters.test.tsx
+++ src/components/MainContainer/FiltersDrawer/Filters/Filters.test.tsx
@@ -1,8 +1,14 @@
 import { shallow } from 'enzyme';
+import { Provider } from 'react-redux';
+import store from '../../../../redux/store';
 
 import Filters from './Filters';
 
 test('should render without any error', () => {
-  const wrapper = shallow(<Filters />);
+  const wrapper = shallow(
+    <Provider store={store}>
+      <Filters />
+    </Provider>,
+  );
   expect(wrapper.isEmptyRender()).toBe(false);
 });
